6e719ebfdc05259767ec72a9fb86a750f47634f3
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-04 00:21:56 -0500

    added home and board routes and helper func

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-
+from flask import Flask, session, render_template
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Acts2:38'
 boggle_game = Boggle()
+
+def init_board():
+  session['board'] = boggle_game.make_board()
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/board')
+def board():
+  init_board()
+
+  return render_template('board.html')
