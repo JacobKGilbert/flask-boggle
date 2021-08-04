bb902b0a97ea85b126413107ffabaea9947c2797
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-04 00:24:17 -0500

    added tests for each route and function

--- test.py
+++ test.py
@@ -8,3 +8,25 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+    def test_home_route(self):
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Boggle Game Home</h1>', html)
+
+    def test_init_board(self):
+        with app.test_client() as client:
+            res = client.get('/board')
+
+            self.assertEqual(res.status_code, 200)
+            self.assertTrue(session['board'])
+
+    def test_board_route(self):
+        with app.test_client() as client:
+            res = client.get('/board')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('test', html)
