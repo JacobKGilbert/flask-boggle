c1d4560b9acc983fbc8a3fc4b5d374af01e61166
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-06 15:29:00 -0500

    added endgame route and before_first_request route for tracking highscore and times played

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ boggle_game = Boggle()
 def init_board():
   session['board'] = boggle_game.make_board()
 
+@app.before_first_request
+def setup():
+  session['high_score'] = 0
+  session['times_played'] = 0
+
 @app.route('/')
 def home():
   return render_template('home.html')
@@ -25,3 +30,15 @@ def guess():
 
   res = boggle_game.check_valid_word(session['board'], guess)
   return res
+
+@app.route('/endgame', methods=['POST'])
+def endgame():
+  req = request.get_json()
+  new_score = req['score']
+
+  if new_score > session['high_score']:
+    session['high_score'] = new_score
+  
+  session['times_played'] += 1
+  
+  return 'ok', 200
